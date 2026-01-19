a613ff67c6bfedac733b3691b0172edd598e6291
pascalaudet <smx867x@outlook.com>
2026-01-18 20:10:17 -0500

    Add project files.

---
+++ HabitWorkbench/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace HabitWorkbench
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new MainPage()) { Title = "HabitWorkbench" };
+        }
+    }
+}
